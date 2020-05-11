c9ea3f52347e1ca20c4eef1f50c41cbe9b414729
brenton <>
2020-05-11 14:13:28 -0400

    implemented Providers to Counter

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,28 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'Counter.dart';
 
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  int count = 0;
-
-  void increment() => setState(() { count++; });
+class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    var count = Provider.of<Counter>(context);
+
     return Scaffold(
       body: new Center(
-        child: new Text(
-          '$count',
-          style: Theme.of(context).textTheme.headline1,
-        ),
+        child: Consumer<Counter>(
+          builder: (context, counter, child) {
+            return new Text(
+              '${counter.getCount()}',
+              style: Theme.of(context).textTheme.headline1,
+            );
+          },
+        )
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: increment,
+        onPressed: count.increment,
         child: new Icon(Icons.add_circle_outline),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'HomePage.dart';
-
+import 'Counter.dart';
 
 void main() => runApp(MainApp());
 
@@ -8,13 +9,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "1. Counter App",
-      home: new HomePage(),
-      theme: new ThemeData(
-        primarySwatch: Colors.green,
-        visualDensity: VisualDensity.adaptivePlatformDensity
-      ),
+    return new ChangeNotifierProvider<Counter>(
+      create: (context) => Counter(),
+      child: new MaterialApp(
+        title: "3a. Counter App (with Providers)",
+        home: new HomePage(),
+        theme: new ThemeData(
+          primarySwatch: Colors.green,
+          visualDensity: VisualDensity.adaptivePlatformDensity
+        ),
+      )
     );
   }
 
